1939bdec84ac4291bb0dca05dcb7cd7a924f9393
Salvador <salvador.sobrino.solorzano.1p@gmail.com>
2025-02-08 19:39:07 +0100

    003 Pom and Data added

---
+++ src/main/java/com/prueba/prices/PricesApplication.java
@@ -0,0 +1,16 @@
+package com.prueba.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesApplication.class, args);
+
+
+	}
+
+}
